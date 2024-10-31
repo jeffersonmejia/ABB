b2626ad8731e380cc190de06c36061eb6d0ca88e
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2024-10-31 13:16:44 -0500

    improve performance

--- src/ABB/Arbol.java
+++ src/ABB/Arbol.java
@@ -6,225 +6,225 @@ import java.util.Scanner;
 
 public class Arbol {
 
-	private Nodo raiz;
-	int info;
-	Nodo izq, der;
-	public boolean graficado = false;
-	Scanner in = new Scanner(System.in);
-	JFrame ventana;
+  private Nodo raiz;
+  int info;
+  Nodo izq, der;
+  public boolean graficado = false;
+  Scanner in = new Scanner(System.in);
+  JFrame ventana;
 
-	/**
-	 * Inicializa la raíz en nulo
-	 */
-	public Arbol() {
-		this.raiz = null;
-	}
+  /**
+   * Inicializa la raíz en nulo
+   */
+  public Arbol() {
+    this.raiz = null;
+  }
 
-	/**
-	 *
-	 * Verifica si el árbol está vacío
-	 *
-	 * @param nodo recibe un nodo
-	 *
-	 * @return devuelve verdadero si está vacío
-	 */
-	public boolean esVacio(Nodo nodo) {
-		return nodo == null;
-	}
+  /**
+   *
+   * Verifica si el árbol está vacío
+   *
+   * @param nodo recibe un nodo
+   *
+   * @return devuelve verdadero si está vacío
+   */
+  public boolean esVacio(Nodo nodo) {
+    return nodo == null;
+  }
 
-	public boolean esVacio() {
-		return this.raiz == null;
-	}
+  public boolean esVacio() {
+    return this.raiz == null;
+  }
 
-	public void insertar(int clave, Object objeto) {
-		if (this.raiz == null) {
-			this.raiz = new Nodo(clave, objeto);
-			System.out.println(">> Nodo insertado con éxito en la raíz padre del árbol");
-			System.out.print("*Escribe 1 para continuar: ");
-			int wait = in.nextInt();
-		} else {
-			this.insertarBinario(clave, objeto, this.raiz);
-		}
-	}
+  public void insertar(int clave, Object objeto) {
+    if (this.raiz == null) {
+      this.raiz = new Nodo(clave, objeto);
+      System.out.println(">> Nodo insertado con éxito en la raíz padre del árbol");
+      System.out.print("*Escribe 1 para continuar: ");
+      int wait = in.nextInt();
+    } else {
+      this.insertarBinario(clave, objeto, this.raiz);
+    }
+  }
 
-	private void insertarBinario(int clave, Object objeto, Nodo puntero) {
-		Nodo nuevo = new Nodo(clave, objeto);
+  private void insertarBinario(int clave, Object objeto, Nodo puntero) {
+    Nodo nuevo = new Nodo(clave, objeto);
 
-		//INSERTA IZQUIERDA
-		if (clave < puntero.getClave()) {
-			if (puntero != null && puntero.getIzquierdo() != null) {
-				// CONVIERTE IZQ EN RAIZ
-				if (puntero == null) {
-					puntero.setIzquierdo(nuevo);
-					System.out.println(">> Nodo insertado con éxito a la izquierda");
-					System.out.print("*Escribe 1 para continuar: ");
-					int wait = in.nextInt();
+    //INSERTA IZQUIERDA
+    if (clave < puntero.getClave()) {
+      if (puntero != null && puntero.getIzquierdo() != null) {
+        // CONVIERTE IZQ EN RAIZ
+        if (puntero == null) {
+          puntero.setIzquierdo(nuevo);
+          System.out.println(">> Nodo insertado con éxito a la izquierda");
+          System.out.print("*Escribe 1 para continuar: ");
+          int wait = in.nextInt();
 
-				}
-				puntero = puntero.getIzquierdo();
-				this.insertarBinario(clave, objeto, puntero);
-			} else if (puntero.getIzquierdo() == null) {
-				puntero.setIzquierdo(nuevo);
-				System.out.println(">> Nodo insertado con éxito a la izquierda");
-				System.out.print("*Escribe 1 para continuar: ");
-				int wait = in.nextInt();
-			}
-			//INSERTAR DERECHA
-		} else if (clave > puntero.getClave()) {
-			if (puntero != null && puntero.getDerecho() != null) {
-				// CONVIERTE IZQ EN RAIZ
-				if (puntero == null) {
-					puntero.setDerecho(nuevo);
-					System.out.println(">> Nodo insertado con éxito a la derecha");
-					System.out.print("*Escribe 1 para continuar: ");
-					int wait = in.nextInt();
-				}
-				puntero = puntero.getDerecho();
-				this.insertarBinario(clave, objeto, puntero);
-			} else if (puntero.getDerecho() == null) {
-				puntero.setDerecho(nuevo);
-				System.out.println(">> Nodo insertado con éxito a la derecha");
-				System.out.print("*Escribe 1 para continuar: ");
-				int wait = in.nextInt();
-			}
+        }
+        puntero = puntero.getIzquierdo();
+        this.insertarBinario(clave, objeto, puntero);
+      } else if (puntero.getIzquierdo() == null) {
+        puntero.setIzquierdo(nuevo);
+        System.out.println(">> Nodo insertado con éxito a la izquierda");
+        System.out.print("*Escribe 1 para continuar: ");
+        int wait = in.nextInt();
+      }
+      //INSERTAR DERECHA
+    } else if (clave > puntero.getClave()) {
+      if (puntero != null && puntero.getDerecho() != null) {
+        // CONVIERTE IZQ EN RAIZ
+        if (puntero == null) {
+          puntero.setDerecho(nuevo);
+          System.out.println(">> Nodo insertado con éxito a la derecha");
+          System.out.print("*Escribe 1 para continuar: ");
+          int wait = in.nextInt();
+        }
+        puntero = puntero.getDerecho();
+        this.insertarBinario(clave, objeto, puntero);
+      } else if (puntero.getDerecho() == null) {
+        puntero.setDerecho(nuevo);
+        System.out.println(">> Nodo insertado con éxito a la derecha");
+        System.out.print("*Escribe 1 para continuar: ");
+        int wait = in.nextInt();
+      }
 
-			//INSERTAR DERECHA
-		} else {
-			System.out.println("(x) Repetido. Nodo no insertado");
-		}
-	}
+      //INSERTAR DERECHA
+    } else {
+      System.out.println("(x) Repetido. Nodo no insertado");
+    }
+  }
 
-	/**
-	 *
-	 * @return devuelve la raiz principal del árbol
-	 */
-	public Nodo getRaiz() {
-		return raiz;
-	}
+  /**
+   *
+   * @return devuelve la raiz principal del árbol
+   */
+  public Nodo getRaiz() {
+    return raiz;
+  }
 
-	public int nodosCompletos(Nodo r) {
-		if (r == null) {
-			return 0;
-		} else {
-			if (r.getIzquierdo() != null && r.getDerecho() != null) {
-				return nodosCompletos(r.getIzquierdo()) + nodosCompletos(r.getDerecho()) + 1;
-			}
-			return nodosCompletos(r.getIzquierdo()) + nodosCompletos(r.getDerecho());
-		}
-	}
+  public int nodosCompletos(Nodo r) {
+    if (r == null) {
+      return 0;
+    } else {
+      if (r.getIzquierdo() != null && r.getDerecho() != null) {
+        return nodosCompletos(r.getIzquierdo()) + nodosCompletos(r.getDerecho()) + 1;
+      }
+      return nodosCompletos(r.getIzquierdo()) + nodosCompletos(r.getDerecho());
+    }
+  }
 
-	/**
-	 *
-	 * Grafica el árbol
-	 */
-	public void graficar() {
-		if (!esVacio(this.raiz)) {
-			Graficar Migrafico = new Graficar(this);
-			this.ventana = new JFrame("Árbol binario");
+  /**
+   *
+   * Grafica el árbol
+   */
+  public void graficar() {
+    if (!esVacio(this.raiz)) {
+      Graficar Migrafico = new Graficar(this);
+      this.ventana = new JFrame("Árbol binario");
 
-			this.ventana.setDefaultCloseOperation(3);
-			this.ventana.setSize(800, 600);
-			this.ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
-			this.ventana.setLocationRelativeTo(null);
-			this.ventana.setBackground(Color.black);
-			this.ventana.getContentPane().add(Migrafico);
-			this.ventana.setVisible(true);
-			graficado = true;
-		}
-	}
+      this.ventana.setDefaultCloseOperation(3);
+      this.ventana.setSize(800, 600);
+      this.ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
+      this.ventana.setLocationRelativeTo(null);
+      this.ventana.setBackground(Color.black);
+      this.ventana.getContentPane().add(Migrafico);
+      this.ventana.setVisible(true);
+      graficado = true;
+    }
+  }
 
-	public void visibilizar(boolean status) {
-		if (status) {
-			this.ventana.setVisible(true);
-		} else {
-			this.ventana.setVisible(false);
-		}
-	}
+  public void visibilizar(boolean status) {
+    if (status) {
+      this.ventana.setVisible(true);
+    } else {
+      this.ventana.setVisible(false);
+    }
+  }
 
-	/**
-	 *
-	 * Llama al método que aplica la lógica de ordenamiento
-	 */
-	public void mostraror() {
-		mostraor(this.raiz);
-	}
+  /**
+   *
+   * Llama al método que aplica la lógica de ordenamiento
+   */
+  public void mostraror() {
+    mostraor(this.raiz);
+  }
 
-	/**
-	 *
-	 * Muestra los nodos del árbol en orden descendente
-	 *
-	 * @param nodo recibe un nodo inicial
-	 */
-	public void mostraor(Nodo nodo) {
-		if (!esVacio(nodo)) {
-			mostraor(nodo.getDerecho());
-			System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
-			mostraor(nodo.getIzquierdo());
-		}
-	}
+  /**
+   *
+   * Muestra los nodos del árbol en orden descendente
+   *
+   * @param nodo recibe un nodo inicial
+   */
+  public void mostraor(Nodo nodo) {
+    if (!esVacio(nodo)) {
+      mostraor(nodo.getDerecho());
+      System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
+      mostraor(nodo.getIzquierdo());
+    }
+  }
 
-	/**
-	 *
-	 * Muestra los nodos del subarbol izquierdo de la raiz
-	 */
-	public void mostrarIzq() {
-		mostraIzq(this.raiz.getIzquierdo());
-	}
+  /**
+   *
+   * Muestra los nodos del subarbol izquierdo de la raiz
+   */
+  public void mostrarIzquierdo() {
+    mostraIzquierdo(this.raiz.getIzquierdo());
+  }
 
-	// metedo con recursividad
-	public void mostraIzq(Nodo nodo) {
-		if (!esVacio(nodo)) {
-			if (nodo.getClave() < raiz.getClave()) {
-				mostraIzq(nodo.getDerecho());
-			}
-			mostraIzq(nodo.getIzquierdo());
-			System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
-		}
-	}
+  // metedo con recursividad
+  public void mostraIzquierdo(Nodo nodo) {
+    if (!esVacio(nodo)) {
+      if (nodo.getClave() < raiz.getClave()) {
+        mostraIzquierdo(nodo.getDerecho());
+      }
+      mostraIzquierdo(nodo.getIzquierdo());
+      System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
+    }
+  }
 
-	/**
-	 *
-	 * Metodo que sera llamado en la clase principal
-	 */
-	public void mostrarDerec() {
-		mostrarDerec(this.raiz.getDerecho());
-	}
+  /**
+   *
+   * Metodo que sera llamado en la clase principal
+   */
+  public void mostrarDerecho() {
+    mostrarDerecho(this.raiz.getDerecho());
+  }
 
-	//metodo recursividad
-	public void mostrarDerec(Nodo nodo) {
-		if (!esVacio(nodo)) {
-			if (nodo.getClave() > raiz.getClave()) {
-				mostrarDerec(nodo.getDerecho());
-			}
-			mostraIzq(nodo.getIzquierdo());
-			System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
-		}
-	}
+  //metodo recursividad
+  public void mostrarDerecho(Nodo nodo) {
+    if (!esVacio(nodo)) {
+      if (nodo.getClave() > raiz.getClave()) {
+        mostrarDerecho(nodo.getDerecho());
+      }
+      mostrarDerecho(nodo.getIzquierdo());
+      System.out.print("Clave: " + nodo.getClave() + " | Objeto: " + nodo.getObjeto() + " | ");
+    }
+  }
 
-	/**
-	 * @param referencia valor del nodo
-	 */
-	public void consultarPorReferencia(int referencia) {
-		consultarPorReferencia(this.raiz, referencia);
-	}
+  /**
+   * @param referencia valor del nodo
+   */
+  public void consultarPorReferencia(int referencia) {
+    consultarPorReferencia(this.raiz, referencia);
+  }
 
-	/**
-	 * @param referencia valor del nodo
-	 * @param nodo apunta al siguiente nodo para avanzar
-	 */
-	private void consultarPorReferencia(Nodo puntero, int referencia) {
-		if (puntero != null) {
-			if (puntero.getClave() == referencia) {
-				System.out.print("Clave: " + puntero.getClave() + " | Objeto: " + puntero.getObjeto() + " ");
-			} else {
-				if (referencia < puntero.getClave()) {
-					consultarPorReferencia(puntero.getIzquierdo(), referencia);
-				} else {
-					consultarPorReferencia(puntero.getDerecho(), referencia);
-				}
-			}
-		} else {
-			System.out.println("(x) El nodo ingresado no existe");
-		}
-	}
+  /**
+   * @param referencia valor del nodo
+   * @param nodo apunta al siguiente nodo para avanzar
+   */
+  private void consultarPorReferencia(Nodo puntero, int referencia) {
+    if (puntero != null) {
+      if (puntero.getClave() == referencia) {
+        System.out.print("Clave: " + puntero.getClave() + " | Objeto: " + puntero.getObjeto() + " ");
+      } else {
+        if (referencia < puntero.getClave()) {
+          consultarPorReferencia(puntero.getIzquierdo(), referencia);
+        } else {
+          consultarPorReferencia(puntero.getDerecho(), referencia);
+        }
+      }
+    } else {
+      System.out.println("(x) El nodo ingresado no existe");
+    }
+  }
 }
--- src/ABB/Graficar.java
+++ src/ABB/Graficar.java
@@ -7,53 +7,53 @@ import javax.swing.JLabel;
 
 public class Graficar extends JPanel {
 
-	//ATRIBUTOS
-	private final Arbol arbol;
-	public static final int DIAMETRO = 40;
-	public static int RADIO = DIAMETRO / 2;
-	public static final int ANCHO = 50;
-	private JLabel label;
+  //ATRIBUTOS
+  private final Arbol arbol;
+  public static final int DIAMETRO = 40;
+  public static int RADIO = DIAMETRO / 2;
+  public static final int ANCHO = 50;
+  private JLabel label;
 
-	//CONSTRUCTOR
-	public Graficar(Arbol miArbol) {
-		this.arbol = miArbol;
-		this.setBackground(Color.black);
-		label = new JLabel("ABB");
-		label.setForeground(Color.white);
-		label.setBounds(0, 0, 0, 0);
-		add(label);
-		repaint();
-	}
+  //CONSTRUCTOR
+  public Graficar(Arbol miArbol) {
+    this.arbol = miArbol;
+    this.setBackground(Color.black);
+    label = new JLabel("ABB");
+    label.setForeground(Color.white);
+    label.setBounds(0, 0, 0, 0);
+    add(label);
+    repaint();
+  }
 
-	//MÉTODO SOBRESCRITO
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		pintar(g, getWidth() / 2, 35, arbol.getRaiz());
-	}
+  //MÉTODO SOBRESCRITO
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+    pintar(g, getWidth() / 2, 35, arbol.getRaiz());
+  }
 
-	//MÉTODO PRIVADO
-	private void pintar(Graphics graphic, int x, int y, Nodo nodo) {
-		if (nodo == null) {
-			return;
-		}
-		graphic.setColor(Color.blue);
-		int EXTRA = new Arbol().nodosCompletos(nodo) * (ANCHO / 2);
+  //MÉTODO PRIVADO
+  private void pintar(Graphics graphic, int x, int y, Nodo nodo) {
+    if (nodo == null) {
+      return;
+    }
+    graphic.setColor(Color.blue);
+    int EXTRA = new Arbol().nodosCompletos(nodo) * (ANCHO / 2);
 
-		graphic.fillOval(x, y, DIAMETRO, DIAMETRO);
-		graphic.setColor(Color.white);
-		graphic.drawString(nodo.getClave() + "", x + 12, y + 23);
-		graphic.setColor(Color.blue);
+    graphic.fillOval(x, y, DIAMETRO, DIAMETRO);
+    graphic.setColor(Color.white);
+    graphic.drawString(nodo.getClave() + "", x + 12, y + 23);
+    graphic.setColor(Color.blue);
 
-		if (nodo.getIzquierdo() != null) {
-			graphic.drawLine(x + RADIO - 5, y + RADIO + 15, x - ANCHO - EXTRA + RADIO, y - 15 + ANCHO + RADIO);
-		}
+    if (nodo.getIzquierdo() != null) {
+      graphic.drawLine(x + RADIO - 5, y + RADIO + 15, x - ANCHO - EXTRA + RADIO, y - 15 + ANCHO + RADIO);
+    }
 
-		if (nodo.getDerecho() != null) {
-			graphic.drawLine(x + RADIO - 5, y + RADIO + 15, x + ANCHO + EXTRA + RADIO, y - 15 + ANCHO + RADIO);
-		}
+    if (nodo.getDerecho() != null) {
+      graphic.drawLine(x + RADIO - 5, y + RADIO + 15, x + ANCHO + EXTRA + RADIO, y - 15 + ANCHO + RADIO);
+    }
 
-		pintar(graphic, x - ANCHO - EXTRA, y + ANCHO, nodo.getIzquierdo());
-		pintar(graphic, x + ANCHO + EXTRA, y + ANCHO, nodo.getDerecho());
-	}
+    pintar(graphic, x - ANCHO - EXTRA, y + ANCHO, nodo.getIzquierdo());
+    pintar(graphic, x + ANCHO + EXTRA, y + ANCHO, nodo.getDerecho());
+  }
 }
